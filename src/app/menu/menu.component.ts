38b9dc7c343bce3961dcd4912a04a7b8a01f0fff
David Guerrero Gomez <david.guerrero.external@atos.net>
2023-05-29 12:59:19 +0200

    Estilos para la cabecera

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../services/usuario.service';
+import { Usuario } from '../interfaces/usuario';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  usuarioActivo!:Usuario
+  usuariosPendientes:number=0;
+
+  constructor(protected userService:UsuarioService){}
+  
+  ngOnInit(): void {
+    
+  }
 
 }
