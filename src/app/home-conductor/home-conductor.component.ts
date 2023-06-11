f3e99683f05f41e236eefa0b06e8d1b71a6185e6
David Guerrero Gómez <95078399+DaGuerGom@users.noreply.github.com>
2023-06-11 16:57:26 +0200

    Portada completa

--- src/app/home-conductor/home-conductor.component.ts
+++ src/app/home-conductor/home-conductor.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-home-conductor',
   templateUrl: './home-conductor.component.html',
   styleUrls: ['./home-conductor.component.css']
 })
-export class HomeConductorComponent {
-
+export class HomeConductorComponent extends MenuComponent{
+  
 }
